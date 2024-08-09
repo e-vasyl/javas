24c12c743308100796756f65718dfb323130c61b
Vasyl Ievdokymov <e.vasyl@gmail.com>
2024-08-09 13:49:50 +0200

    spring-boot-rest initial commit

---
+++ spring-boot-hello/spring-boot-rest/src/main/java/org/example/dto/HelloDTO.java
@@ -0,0 +1,13 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+@Value
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+public class HelloDTO {
+    String message;
+
+}
